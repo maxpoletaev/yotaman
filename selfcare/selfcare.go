6d0547132c503dbaa03048ac948b27d3cf228bc7
Maxim Poletaev <max.poletaev@gmail.com>
2016-05-30 00:53:54 +0300

    Codestyle fix

--- selfcare/selfcare.go
+++ selfcare/selfcare.go
@@ -1,41 +1,42 @@
 package selfcare
 
 import (
-    "net/url"
-    "net/http"
-    "net/http/cookiejar"
+	"net/url"
+	"net/http"
+	"net/http/cookiejar"
 )
 
 const (
-    CHANGE_OFFER_URL string = "https://my.yota.ru/selfcare/devices/changeOffer"
-    DEVICES_URL string = "https://my.yota.ru/selfcare/devices"
-    LOGIN_URL string = "https://login.yota.ru/UI/Login"
-    AUTO_LOGIN_URL string = "https://my.yota.ru/selfcare/mydevices"
-    LOGIN_SUCCESS_URL string = "https://my.yota.ru/selfcare/loginSuccess"
-    LOGIN_ERROR_URL string = "https://my.yota.ru:443/selfcare/loginError"
+	CHANGE_OFFER_URL = "https://my.yota.ru/selfcare/devices/changeOffer"
+	DEVICES_URL = "https://my.yota.ru/selfcare/devices"
+	LOGIN_URL = "https://login.yota.ru/UI/Login"
+	AUTO_LOGIN_URL = "https://my.yota.ru/selfcare/mydevices"
+	LOGIN_SUCCESS_URL = "https://my.yota.ru/selfcare/loginSuccess"
+	LOGIN_ERROR_URL = "https://my.yota.ru:443/selfcare/loginError"
 )
 
 func Login(username string, password string) {
-    _, err := client.PostForm(LOGIN_URL, url.Values{
-        "gotoOnFail": {LOGIN_ERROR_URL},
-        "goto": {LOGIN_SUCCESS_URL},
-        "ForceAuth": {"true"},
-        "org": {"customer"},
-        "IDToken1": {username},
-        "IDToken2": {password},
-    })
-    if err != nil { panic(err) }
+	form := url.Values{
+		"gotoOnFail": {LOGIN_ERROR_URL},
+		"goto": {LOGIN_SUCCESS_URL},
+		"ForceAuth": {"true"},
+		"org": {"customer"},
+		"IDToken1": {username},
+		"IDToken2": {password},
+	}
+	_, err := client.PostForm(LOGIN_URL, form)
+	if err != nil { panic(err) }
 }
 
 func AutoLogin() {
-    _, err := client.Get(AUTO_LOGIN_URL)
-    if err != nil { panic(err) }
+	_, err := client.Get(AUTO_LOGIN_URL)
+	if err != nil { panic(err) }
 }
 
 func CreateClient() http.Client {
-    jar, _ := cookiejar.New(nil)
-    client := http.Client{Jar: jar}
-    return client
+	jar, _ := cookiejar.New(nil)
+	client := http.Client{Jar: jar}
+	return client
 }
 
 var client http.Client = CreateClient()
--- yotaman.go
+++ yotaman.go
@@ -1,61 +1,61 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "github.com/spf13/cobra"
-    "./selfcare"
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+	"github.com/zenwalker/yotaman/selfcare"
 )
 
 type Config struct {
-    username string
-    password string
+	username string
+	password string
 }
 
 var RootCmd = &cobra.Command{
-    Use: "yotaman",
+	Use: "yotaman",
 }
 
 var ListTariffCmd = &cobra.Command{
-    Use: "list",
-    Short: "Show avaliable tariffs",
-    Run: func(cmd *cobra.Command, args []string) {
-        selfcare.AutoLogin()
-        device := selfcare.GetCurrentDevice()
-        for _, tariff := range device.Tariffs {
-            var currentFlag string
-            if device.IsCurrentTariff(tariff) {
-                currentFlag = ">"
-            }
-            output := fmt.Sprintf("%1s %4s | %s", currentFlag, tariff.Label(), tariff.Repr())
-            fmt.Println(output)
-        }
-    },
+	Use: "list",
+	Short: "Show avaliable tariffs",
+	Run: func(cmd *cobra.Command, args []string) {
+		selfcare.AutoLogin()
+		device := selfcare.GetCurrentDevice()
+		for _, tariff := range device.Tariffs {
+			var currentFlag string
+			if device.IsCurrentTariff(tariff) {
+				currentFlag = ">"
+			}
+			output := fmt.Sprintf("%1s %4s | %s", currentFlag, tariff.Label(), tariff.Repr())
+			fmt.Println(output)
+		}
+	},
 }
 
 var SetTariffCmd = &cobra.Command{
-    Use: "set",
-    Short: "Change current tariff",
-    Run: func(cmd *cobra.Command, args []string) {
-        selfcare.AutoLogin()
-        newLabel := args[0] // TODO: check exists
+	Use: "set",
+	Short: "Change current tariff",
+	Run: func(cmd *cobra.Command, args []string) {
+		selfcare.AutoLogin()
+		newLabel := args[0] // TODO: check exists
 
-        device := selfcare.GetCurrentDevice()
-        for _, tariff := range device.Tariffs {
-            if tariff.Label() == newLabel {
-                device.ChangeTariff(tariff)
-                break
-            }
-        }
-    },
+		device := selfcare.GetCurrentDevice()
+		for _, tariff := range device.Tariffs {
+			if tariff.Label() == newLabel {
+				device.ChangeTariff(tariff)
+				break
+			}
+		}
+	},
 }
 
 func main() {
-    RootCmd.AddCommand(ListTariffCmd)
-    RootCmd.AddCommand(SetTariffCmd)
+	RootCmd.AddCommand(ListTariffCmd)
+	RootCmd.AddCommand(SetTariffCmd)
 
-    if err := RootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(-1)
-    }
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
 }
